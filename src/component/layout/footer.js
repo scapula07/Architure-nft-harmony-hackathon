de9a84d45af612d8cbbdd01f58b4908eb64fc5cc
coolpythoncodes <coolpythoncodes@gmail.com>
2022-10-02 00:09:21 +0100

    modified the footer

--- src/component/layout/footer.js
+++ src/component/layout/footer.js
@@ -1,20 +1,28 @@
-import Logo from "assests/footer/logo.png"
+import { footerLinks } from "utils/data";
+
 
 const Footer = () => {
-    return (
-        <footer className="bg-b1 text-white py-10">
-            <div className="layout-container">
-                {/* logo and terms of condition */}
-                <div className="space-y-2">
-                    <img src={Logo} alt="" />
-                    <ul className="capitalize space-x-3">
-                        <a href="/">Privacy policy</a>
-                        <a href="/">Terms of service</a>
-                    </ul>
-                </div>
-            </div>
-        </footer>
-    )
+  const year = new Date().getFullYear();
+  return (
+    <footer className='layout-container pb-8 md:flex md:justify-between items-center text-white'>
+      <div className='text-center md:text-left'>
+        <div className="font-medium text-xl leading-6 uppercase mb-2">Vitruvius</div>
+        <p className="text-sm leading-6">Copyright © {year} Scapula Labs, Inc. <br /> All rights reserved.</p>
+      </div>
+      {/* footer links */}
+      <div className='space-x-4 mt-2 text-center'>
+        {footerLinks.map((url, index) => (
+          <a
+            key={index}
+            href={url.link}
+            className="font-medium text-base leading-[30px]"
+          >
+            {url.name}
+          </a>
+        ))}
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
--- src/utils/data/index.js
+++ src/utils/data/index.js
@@ -1,3 +1,4 @@
 export * from './navlinks.data'
 export * from './new-collection.data'
 export * from './top-nft-creators.data'
+export * from './footerLinks.data'
