0844e44ef3c53e1293024f3ccac2e3e91b58bd52
goodness065 <goodnesskelechi2017@yahoo.com>
2022-10-15 23:35:50 -0700

    worked on goverance page

--- src/pages/goverance/votesBreakdown.js
+++ src/pages/goverance/votesBreakdown.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import { voteBreakdownData } from "utils/data"
+import { votesBreakdownData } from "utils/data";
 
 const VotesBreakdown = () => {
-    return (
-        <div className='bg-[#2D2D2D] rounded-[8px] py-8 px-[22px] mt-4 w-full'>
-            <h2 className='capitalize text-[#F5F5F5] text-base leading-[18px] font-medium mb-2'>Vote breakdown</h2>
-            {
-                voteBreakdownData.map((item, index) => (
-                    <div>
-                        <p className='text-[#D9D9D9] text-sm leading-7 mb-4'>You are about to send 1 transaction(s) to the blockchain, Sign a message in your wallet to mint and list this item</p>
-                        <p className='text-[#D9D9D9] text-sm leading-6'>LEADING OPTION: <span className='text-[#66A8FF] uppercase'> Wiz buildings </span> with 23,243 ONE Supporting</p>
-
-                    </div>
-                ))
-            }
-
+  return (
+    <div className="bg-[#2D2D2D] rounded-[8px] py-8 px-[22px] mt-4 w-full">
+      <h2 className="capitalize text-[#F5F5F5] text-base leading-[18px] font-medium mb-2">
+        Vote breakdown
+      </h2>
+      {votesBreakdownData.map((item, index) => (
+        <div key={index}>
+          <div className="flex justify-between items-center w-full p-[0]">
+            <p className="text-[#D9D9D9] text-sm leading-7">{item.name}</p>
+            <p className="text-[#D9D9D9] text-sm leading-6">
+              {item.amount} ONE voting ({item.percentage}%)
+            </p>
+          </div>
+          <div className="py-[8px]">
+          <progress id="file" value={item.percentage} max="100" className="w-full !rounded-[10px]"/>
+          </div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default VotesBreakdown
+export default VotesBreakdown;
